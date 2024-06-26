25a547797c9bf05e0ea6a699973b9e01d9e92d7f
Abubakar Rasheed <131538521+AbubakarTechy@users.noreply.github.com>
2024-06-26 22:51:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //     videoModal.classList.add('video-modal');
 //     videoModal.innerHTML = `
 
+
 //         <div class="video-content">
 //             <span class="close-button">&times;</span>
 //             <video controls autoplay>
